00bfd136154693d0ca15814e6b242727bb6b0ee2
TakshakRamteke <takshakramteke0708@gmail.com>
2022-07-11 23:53:15 +0530

    fix : TG incharge sidebar added  link to tg's page

--- components/Sidebar/TgSidebar.tsx
+++ components/Sidebar/TgSidebar.tsx
@@ -111,6 +111,33 @@ const Sidebar = () => {
         </Link>
       </div>
 
+      {session?.role == UserRole.INCHARGE && (
+        <div
+          className={`my-5 ${
+            isActive(`/${session?.role}/tgs`)
+              ? "bg-purple-500 rounded-md p-2 text-white"
+              : ""
+          }`}>
+          <Link href={`/${session?.role}/tgs`}>
+            <a>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-7 w-7'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth={2}>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z'
+                />
+              </svg>
+            </a>
+          </Link>
+        </div>
+      )}
+
       <div
         className={`my-5 ${
           isActive(`/${session?.role}/students`)
@@ -126,10 +153,12 @@ const Sidebar = () => {
               viewBox='0 0 24 24'
               stroke='currentColor'
               strokeWidth={2}>
+              <path d='M12 14l9-5-9-5-9 5 9 5z' />
+              <path d='M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z' />
               <path
                 strokeLinecap='round'
                 strokeLinejoin='round'
-                d='M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z'
+                d='M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222'
               />
             </svg>
           </a>

7c7adf79b79589d08fae688a50b18d4b9522f5dd
TakshakRamteke <takshakramteke0708@gmail.com>
2022-07-13 23:17:01 +0530

    Feat : Create & delete TG

--- pages/INCHARGE/tgs/[slug].tsx
+++ pages/INCHARGE/tgs/[slug].tsx
@@ -61,6 +61,17 @@ const SingleTgPage = ({ tg, students }: TgPageProps) => {
     }
   };
 
+  const handleDelete = async () => {
+    try {
+      const res = await axios.post("/api/incharge/deleteTg", { data: tg.id });
+      if (res.status == 200) {
+        router.push("/INCHARGE/tgs");
+      }
+    } catch (error) {
+      //Handle Error
+    }
+  };
+
   useEffect(() => {
     if (searchQuery.length >= 3) {
       handleSearch();
@@ -279,6 +290,24 @@ const SingleTgPage = ({ tg, students }: TgPageProps) => {
               </tr>
             ))}
           </Table>
+          <button
+            className='flex items-center font-semibold bg-red-600 p-2 rounded-md text-white mt-10'
+            onClick={() => handleDelete()}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6 pr-1'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              strokeWidth={2}>
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16'
+              />
+            </svg>
+            Delete TG
+          </button>
         </div>
       </main>
     </Layout>
